f953efb9f95be6ac57174bcc7deb33bb6dd5ed30
Ryan Liu <isryanyhliu@foxmail.com>
2023-12-13 11:47:33 +0000

    1

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Y.Liu331@liverpool.ac.uk-RockPaperScissors-1.8";
+const contentToCache = [
+    "Build/V1.8.loader.js",
+    "Build/V1.8.framework.js.gz",
+    "Build/V1.8.data.gz",
+    "Build/V1.8.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
